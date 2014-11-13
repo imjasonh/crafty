912d5b4f4e2a410a5c01ebe31e1cd6ae51445ad8
Jason Hall <imjasonh@gmail.com>
2014-11-12 20:48:34 -0500

    split JS into its own file

--- update_gcs.sh
+++ update_gcs.sh
@@ -1,2 +1,4 @@
-gsutil -h "Content-Type: text/html" cp palette gs://www.imjasonh.com/palette/
-gsutil -m cp *.png gs://www.imjasonh.com/palette/
+dest=gs://www.imjasonh.com/palette/
+gsutil -h "Content-Type: text/html" cp palette $dest
+gsutil -m cp *.png $dest
+gsutil -m cp *.js $dest
